1049289c2fe6369c780b8a698262d40123381f1d
kontrybutor <r.massarnowski@gmail.com>
2020-07-14 12:17:45 +0200

    Class Item filled

--- inc/Item.hpp
+++ inc/Item.hpp
@@ -1,1 +1,31 @@
 #pragma once
+
+#include "Cargo.hpp"
+
+enum class Rarity {
+    Common,
+    Rare,
+    Epic,
+    Legendary,
+};
+
+class Item : public Cargo {
+public:
+    Item(size_t amount, const std::string& name, size_t basePrice, Rarity rarity)
+        : Cargo(amount, name, basePrice), rarity_(rarity) {}
+
+    ~Item();
+
+    // override from Cargo
+    std::string getName() const override { return name_; }
+    size_t getAmount() const override { return amount_; }
+    size_t getPrice() const override;
+    size_t getBasePrice() const override { return basePrice_; }
+
+    bool operator==(const Cargo& cargo) const override;
+    Cargo& operator+=(size_t amount) override;
+    Cargo& operator-=(size_t amount) override;
+
+private:
+    Rarity rarity_;
+};
--- src/Item.cpp
+++ src/Item.cpp
@@ -1,1 +1,29 @@
 #include "Item.hpp"
+
+#include <iostream>
+
+bool Item::operator==(const Cargo& cargo) const {
+    auto fruit = static_cast<const Item*>(&cargo);
+    return name_ == fruit->getName() && amount_ == fruit->getAmount() && basePrice_ == fruit->getBasePrice();
+}
+
+Cargo& Item::operator+=(size_t amount) {
+    amount_ += amount;
+    return *this;
+}
+
+Cargo& Item::operator-=(size_t amount) {
+    if (amount > amount_) {
+        amount_ = 0;
+    } else if (amount_ > 0) {
+        amount_ -= amount;
+    } else {
+        std::cerr << "Amount equal zero\n";
+    }
+    return *this;
+}
+
+size_t Item::getPrice() const
+{
+    return basePrice_ * static_cast<int>(rarity_);
+}
